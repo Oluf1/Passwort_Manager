26edd13c8e061183676daaaa2f8bceabb20a688f
ItzOluf <itzoluf@proton.me>
2026-04-14 18:50:42 +0200

    cleaned up pagination

--- main.py
+++ main.py
@@ -4,27 +4,20 @@ from tkinter import ttk
 import tkinter.font as tkfont
 from encrypt import encrypt
 from decrypt import decrypt
-
+from typing import Callable
 
 class App:
     def __init__(self):
         self.root = tk.Tk()
         self.root.geometry("600x400")
-        self.vaults ={}
         with open("config.json") as f:
             self.vaults = json.load(f)["Vaults"]
-        #for ele in self.vaults:
-         #   self.vaults[ele]["directories"][0]
-          #  self.vaults[ele]["directories"][1]
-        #with open("exampledata.json") as f:
-        #    self.database = json.load(f)
-
-        #self.existing_services = [
-        #    (entry["Service"], entry["Mail"], entry["count"])
-        #    for entry in self.database["Entries"]
-        #]
-        
-        
+        self.vault_names = []
+        for name in self.vaults:
+            self.vault_names.append(name)
+        self.selected_vault =""
+        self.selected_service=""
+        self.items_per_page = 18 # will be in config later 
         border_width = 2
         self.main_frame = ttk.Frame(self.root,borderwidth=border_width,relief="solid")
         self.main_frame.place(relheight=1,relwidth=0.2,relx=0,rely=0)
@@ -68,58 +61,65 @@ class App:
         name_label = tk.Label(self.main_frame)
         open_vaults_button = tk.Button(self.main_frame,
                                        text="Vaults",bg="royalblue",
-                                       command=self.load_vaults)
+                                       command=lambda: self.load_vaults(0))
         name_label.place(relx=0,rely=0,relwidth=0.95)
         config_button = tk.Button(self.main_frame,text="config",bg="lightgrey")
         open_vaults_button.place(relx=0,rely=0.1,relwidth=1,relheight=0.1,)
         config_button.place(relx=0,rely=0.21,relwidth=1,relheight=0.1)
         self.root.after(10, lambda: self.fit_font(name_label, "Password Manager"))
         
-    def load_vaults(self):
-        self.vault_buttons : list[tk.Button] = []
-        counter = 0
-        for vault in self.vaults:
-            if counter ==0:
-                new_vault_button = tk.Button(self.subframe_1,
-                                             command=self.add_vault,
-                                             text="New Vault")
-                self.vault_buttons.append(new_vault_button)
-                counter+=1
-            elif counter%20 ==0:
-                up_button = tk.Button(self.subframe_1,text="previous page",
-                                      command= lambda page = counter//18 -1: self.place_Vaults(page))
-                self.vault_buttons.append(up_button)
-                counter+=1
-            elif (counter+1) % 20 ==0 and counter/20 != len(self.vaults)/18:
-                down_button = tk.Button(self.subframe_1,text="next page",
-                                      command= lambda page = counter//18 +1: self.place_Vaults(page))
-                self.vault_buttons.append(down_button)
-                counter += 1
-            vault_button = tk.Button(self.subframe_1,command= lambda name=vault: self.open_vault(name),text=vault)
-            self.vault_buttons.append(vault_button)
-            counter +=1 
-        self.place_Vaults(0)
+    def load_vaults(self,page:int):
+        self.render_pages(0,self.vault_names,self.subframe_1,self.open_vault)
         
-            
-    def place_Vaults(self,page:int):
-        self.clear_subframes(self.subframe_1)
-        if page == len(self.vaults)//18:
-            for i in range(len(self.vaults)+1):
-                button = self.vault_buttons[i+20*page]
-                button.place(rely=0.05*i,relwidth=1,relheight=0.045)
+        
+        
+    def render_pages(self,page:int,items:list,frame:ttk.Frame,function:Callable):
+        self.clear_subframes(frame)
+        start = page*self.items_per_page
+        end= start+self.items_per_page
+        current_items =items[start:end]
+        if page==0:
+            match frame:
+                case self.subframe_1:
+                    add_vault_button = tk.Button(self.subframe_1,text="Add vault",command=self.add_vault)
+                    add_vault_button.place(relx=0,rely=0,relwidth=1,relheight=0.05)
+                case self.subframe_2:
+                    add_service_button = tk.Button(self.subframe_2,text="Add Service", command=self.add_service)
+                    add_service_button.place(relx=0,rely=0,relwidth=1,relheight=0.05)
+                case self.subframe_3:
+                    pass
+                case _:
+                    print("error wrong subframe in render_pages")
         else:
-            for i in range(20):
-                button = self.vault_buttons[i+20*page]
-                button.place(rely=0.05*i,relwidth=1,relheight=0.045)
+            up_button = tk.Button(frame,text="page up",command= lambda:self.render_pages(page-1,items,frame,function))
+            up_button.place(relx=0,rely=0,relwidth=1,relheight=0.05)
+        for i,name in enumerate(current_items):
+            button = tk.Button(frame,text=name,command=function(name))
+            button.place(relheight=0.05,relx=0,relwidth=1,rely=0.05+0.05*i)
     def clear_subframes(self,subframe:ttk.Frame):
             for widget in subframe.winfo_children():
-                widget.place_forget() #type: ignore
+                widget.destroy() 
     def add_vault(self): 
         pass
     def open_vault(self,name:str):
-        print(name)
-        
-
-
+        self.selected_vault = name
+        self.clear_subframes(self.subframe_2)
+        vault = self.vaults[name]
+        services = set()
+        if vault["type"] == "local":
+            location = vault["directories"][1]
+            with open(location) as f:
+                Entries = json.load(f)["Entries"]
+                for entry in Entries:
+                    services.add(entry["Service"])
+                
+            
+                    
+        elif vault["type"] == "server":
+            print("not yet implemented")
+        else:
+            print(f"{vault['type']} is not a valid save type.")
+    def add_service(self):
+        pass
 if __name__ == "__main__":
     App()
