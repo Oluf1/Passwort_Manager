e46274d610117c98b10580930b2a9a1b72443d41
ItzOluf <itzoluf@proton.me>
2026-04-22 01:05:16 +0200

    encrypting and decrypting passwords
    To Do: add update_existing passwords

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ import tkinter.font as tkfont
 from typing import Callable
 from tkinter import messagebox
 from tkinter import filedialog
+from tkinter import simpledialog
+from decrypt import decrypt
+from encrypt import encrypt
+
 class App:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Password Manager")
-        
-        
+
         with open("config.json") as f:
             self.vaults = json.load(f)["Vaults"]
         self.vault_names = []
@@ -80,7 +83,7 @@ class App:
         self.root.after(10, lambda: self.fit_font(name_label, "Password Manager"))
 
     def load_vaults(self, page: int):
-        self.selected_vault =""
+        self.selected_vault = ""
         self.render_pages(0, self.vault_names, self.subframe_1, self.open_vault)
 
     def render_pages(
@@ -89,7 +92,7 @@ class App:
         self.clear_subframes(frame)
         start = page * self.items_per_page
         end = start + self.items_per_page
-        btn_size = 1/ (self.items_per_page +2)
+        btn_size = 1 / (self.items_per_page + 2)
         current_items = items[start:end]
         if page == 0:
             match frame:
@@ -97,37 +100,50 @@ class App:
                     new_vault_button = tk.Button(
                         self.subframe_1, text="Add vault", command=self.new_vault
                     )
-                    new_vault_button.place(relx=0, rely=0, relwidth=1, relheight=btn_size)
+                    new_vault_button.place(
+                        relx=0, rely=0, relwidth=1, relheight=btn_size
+                    )
                 case self.subframe_2:
                     add_service_button = tk.Button(
                         self.subframe_2, text="Add Service", command=self.add_service
                     )
-                    add_service_button.place(relx=0, rely=0, relwidth=1, relheight=btn_size)
+                    add_service_button.place(
+                        relx=0, rely=0, relwidth=1, relheight=btn_size
+                    )
                 case self.subframe_3:
                     add_mail_button = tk.Button(
-                        self.subframe_3,text="add Mail",command=self.add_mail
+                        self.subframe_3, text="add Mail", command=self.add_mail
                     )
-                    add_mail_button.place(relx=0, rely=0, relwidth=1, relheight=btn_size)
+                    add_mail_button.place(
+                        relx=0, rely=0, relwidth=1, relheight=btn_size
+                    )
                 case _:
                     print("error wrong subframe in render_pages")
         else:
             up_button = tk.Button(
                 frame,
                 text="page up",
-                command=lambda new_page = page-1: self.render_pages(new_page, items, frame, function),
+                command=lambda new_page=page - 1: self.render_pages(
+                    new_page, items, frame, function
+                ),
             )
             up_button.place(relx=0, rely=0, relwidth=1, relheight=btn_size)
-        for i, name in enumerate(current_items):
-            button = tk.Button(frame, text=name, command=lambda temp_name = name: function(temp_name))
-            button.place(relheight=0.05, relx=0, relwidth=1, rely=btn_size + btn_size * i)
-        if len(current_items )> len(items):
+        for i, item in enumerate(current_items):
+            button = tk.Button(
+                frame, text=str(item), command=lambda it=item: function(it)
+            )
+            button.place(
+                relheight=0.05, relx=0, relwidth=1, rely=btn_size + btn_size * i
+            )
+        if len(current_items) > len(items):
             down_button = tk.Button(
                 frame,
                 text="page down",
-                command=lambda new_page = page+1: self.render_pages(new_page,items,frame,function)
+                command=lambda new_page=page + 1: self.render_pages(
+                    new_page, items, frame, function
+                ),
             )
-            down_button.place(relx=0,relheight=btn_size,rely=1-btn_size,relwidth=1)
-
+            down_button.place(relx=0, relheight=btn_size, rely=1 - btn_size, relwidth=1)
 
     def clear_subframes(self, subframe: ttk.Frame):
         for widget in subframe.winfo_children():
@@ -136,61 +152,76 @@ class App:
     def new_vault(self):
         new_vault_popup = tk.Toplevel(self.root)
         new_vault_popup.title("New Vault")
-        new_vault_popup.configure(bg='gray74')
-        self.scale_toplevel(new_vault_popup,0.5)
-        
-        tk.Label(new_vault_popup,text="New vault Name").place(relheight=0.1,relx=0,rely=0)
+        new_vault_popup.configure(bg="gray74")
+        self.scale_toplevel(new_vault_popup, 0.5)
+
+        tk.Label(new_vault_popup, text="New vault Name").place(
+            relheight=0.1, relx=0, rely=0
+        )
         vault_name_entry = tk.Entry(new_vault_popup)
-        
-        vault_name_entry.place(relheight=0.15,relwidth=1,relx=0,rely=0.1)
-        
-        vault_type =tk.StringVar(value="local")
-        
-        tk.Label(new_vault_popup,text="vault type").place(relheight=0.1,relx=0,rely=0.27)
-        tk.Radiobutton(new_vault_popup,text="Local",value="local",variable=vault_type).place(relheight=0.1,relx=0,rely=0.4)
-        tk.Radiobutton(new_vault_popup,text="server",value="server",variable=vault_type).place(relheight=0.1,relx=0.5,rely=0.4)
-        
+
+        vault_name_entry.place(relheight=0.15, relwidth=1, relx=0, rely=0.1)
+
+        vault_type = tk.StringVar(value="local")
+
+        tk.Label(new_vault_popup, text="vault type").place(
+            relheight=0.1, relx=0, rely=0.27
+        )
+        tk.Radiobutton(
+            new_vault_popup, text="Local", value="local", variable=vault_type
+        ).place(relheight=0.1, relx=0, rely=0.4)
+        tk.Radiobutton(
+            new_vault_popup, text="server", value="server", variable=vault_type
+        ).place(relheight=0.1, relx=0.5, rely=0.4)
+
         def add_vault():
             if vault_type.get() == "server":
-                messagebox.showerror("Error","Server saving not yet implemented")
+                messagebox.showerror("Error", "Server saving not yet implemented")
                 return
             vault_name = vault_name_entry.get()
             if vault_name in self.vault_names:
-                messagebox.showerror("Error","Vault name already exists choose another")
+                messagebox.showerror(
+                    "Error", "Vault name already exists choose another"
+                )
                 return
-            save_dir = filedialog.askopenfilename(initialdir="/",
-                                                 title="select save directory",
-                                                 filetypes = (("json files",
-                                                        "*.json*"),))
-            key_dir = filedialog.askopenfilename(initialdir="/",
-                                                 title="select key directory",
-                                                 filetypes = (("Text files",
-                                                        "*.txt*"),))
+            save_dir = filedialog.askopenfilename(
+                initialdir="/",
+                title="select save directory",
+                filetypes=(("json files", "*.json*"),),
+            )
+            key_dir = filedialog.askopenfilename(
+                initialdir="/",
+                title="select key directory",
+                filetypes=(("Text files", "*.txt*"),),
+            )
             if not save_dir or not key_dir:
                 messagebox.showerror("Error", "No directory selected")
                 return
-            with open("config.json")as config:
+            with open("config.json") as config:
                 data = json.load(config)
-            data["Vaults"][vault_name] = {"directories":
-                                    [
-                                        key_dir,
-                                        save_dir
-                                    ],
-                                    "type":"local"
-                                }
+            data["Vaults"][vault_name] = {
+                "directories": [key_dir, save_dir],
+                "type": "local",
+            }
             with open("config.json", "w") as file:
-                json.dump(data,file,indent=2)
+                json.dump(data, file, indent=2)
+            with open(save_dir) as file:
+                save = json.load(file)
             
+            save.setdefault("services", {})
+            with open(save_dir,"w") as file:
+                json.dump(save,file,indent=2)
             new_vault_popup.destroy()
             self.vault_names.append(vault_name)
             self.load_vaults(0)
-        
-        tk.Button(new_vault_popup,text="choose locations",command=add_vault).place(relheight=0.1,rely=0.5,relx=0)
-        
-        
+
+        tk.Button(new_vault_popup, text="choose locations", command=add_vault).place(
+            relheight=0.1, rely=0.5, relx=0
+        )
+
         new_vault_popup.transient(self.root)
         new_vault_popup.grab_set
-        
+
     def open_vault(self, name: str):
         self.selected_vault = name
         self.clear_subframes(self.subframe_2)
@@ -205,7 +236,6 @@ class App:
                 data = json.load(f)
 
                 services = list(data["services"].keys())
-                
 
         elif vault["type"] == "server":
             print("not yet implemented")
@@ -215,16 +245,18 @@ class App:
         self.render_pages(0, services, self.subframe_2, self.open_service)
 
     def add_service(self):
-        new_service_popup = tk.Toplevel(self.root,)
-        self.scale_toplevel(new_service_popup,0.5)
-        
+        new_service_popup = tk.Toplevel(
+            self.root,
+        )
+        self.scale_toplevel(new_service_popup, 0.5)
+
         name_entry = tk.Entry(new_service_popup)
-        name_entry.place(relheight=0.1,relx=0,rely=0.1)
-        tk.Label(new_service_popup,text="Name").place(relheight=0.1,relx=0,rely=0)
-        
+        name_entry.place(relheight=0.1, relx=0, rely=0.1)
+        tk.Label(new_service_popup, text="Name").place(relheight=0.1, relx=0, rely=0)
+
         def create_service():
             new_name = name_entry.get()
-            
+
             services: list[str] = []
             vault = self.vaults[self.selected_vault]
             if vault["type"] == "local":
@@ -238,64 +270,110 @@ class App:
                     return
                 new_service_popup.destroy()
                 data["services"][new_name] = []
-                with open(location,"w") as file:
-                    json.dump(data,file,indent=2) 
+                with open(location, "w") as file:
+                    json.dump(data, file, indent=2)
                 self.open_vault(self.selected_vault)
 
-        
-        tk.Button(new_service_popup,command=create_service,
-                  text="create service").place(relheight=0.1,rely=0.3,relx=0)
-        
-        
-        
+        tk.Button(
+            new_service_popup, command=create_service, text="create service"
+        ).place(relheight=0.1, rely=0.3, relx=0)
+
         new_service_popup.transient(self.root)
         new_service_popup.grab_set
-        
-    def open_service(self,name:str):
+
+    def open_service(self, name: str):
         self.selected_service = name
-        
+
         self.clear_subframes(self.subframe_3)
 
         vault = self.vaults[self.selected_vault]
-        Mails:list[str] = []
+        Mails: list = []
         if vault["type"] == "local":
             location = vault["directories"][1]
             with open(location) as f:
                 data = json.load(f)
-            
-            
+
             for ele in data["services"][name]:
-                Mails.append(ele["Mail"])
-            self.render_pages(0,Mails,self.subframe_3,self.open_mail)
-        
-    def open_mail(self,name:str):
+                Mails.append((ele["Mail"], ele["count"]))
+            self.render_pages(0, Mails, self.subframe_3, self.open_mail)
+
+    def open_mail(self, name: str):
         mail_popup = tk.Toplevel(self.root)
-        
-        
-        self.scale_toplevel(mail_popup,0.5)
+
+        self.scale_toplevel(mail_popup, 0.5)
         mail_popup.title(name)
-        
-        tk.Label(mail_popup,
-                 text=f"service: {self.selected_service}"
-                 ).place(relx=0,rely=0,relheight=0.15)
-        password_Label =tk.Label(mail_popup,text="password: ****")
-        password_Label.place(relx=0,relheight=0.15,rely=0.15)
-        tk.Button(mail_popup,text="decrypt Password").place(relx=0.3,rely=0.15,relheight=0.15)
-        
+
+        tk.Label(mail_popup, text=f"service: {self.selected_service}").place(
+            relx=0, rely=0, relheight=0.15
+        )
+        password_Label = tk.Label(mail_popup, text="password: ****")
+        password_Label.place(relx=0, relheight=0.15, rely=0.15)
+
+        def decrypt_password():
+            master_password = simpledialog.askstring(
+                "enter Masterpassword", "Masterpassword:"
+            )
+            if master_password is not None:
+                master_password = master_password.encode("utf-8")
+            else:
+                messagebox.showerror("Error", "Masterpassword can not be empty")
+                return
+            Mail = name[0]
+            count = int(name[1])
+            decrypted_password = decrypt(
+                master_password, self.selected_service, Mail, count, self.selected_vault
+            )
+            password_Label.configure(text=decrypted_password)
+
+        tk.Button(mail_popup, text="decrypt Password", command=decrypt_password).place(
+            relx=0.3, rely=0.15, relheight=0.15
+        )
+
         mail_popup.transient(self.root)
         mail_popup.grab_set
-    def scale_toplevel(self,window:tk.Toplevel,size:float):
+
+    def add_mail(self):
+        add_mail_popup = tk.Toplevel(self.root)
+        self.scale_toplevel(add_mail_popup,0.5)
+        
+        name_entry = tk.Entry(add_mail_popup)
+        tk.Label(add_mail_popup,text="Email").place(relx=0,rely=0,relheight=0.15)
+        name_entry.place(relheight=0.15,relx=0,rely=0.15)
+        
+        password_entry = tk.Entry(add_mail_popup)
+        tk.Label(add_mail_popup,text="Password").place(relx=0,relheight=0.15,rely=0.3)
+        password_entry.place(relx=0,rely=0.45,relheight=0.15)
+        
+        master_password_entry = tk.Entry(add_mail_popup)
+        tk.Label(add_mail_popup,text="Masterpassword").place(relx=0,rely=0.6,relheight=0.1)
+        master_password_entry.place(relx=0,rely=0.7,relheight=0.1)
+        
+        def add_entry():
+            name= name_entry.get()
+            service = self.selected_service
+            vault = self.selected_vault
+            password = password_entry.get()
+            master = master_password_entry.get()
+            encrypt(master.encode(),password.encode(),service,vault,name,1,False)
+            add_mail_popup.destroy()
+            self.open_service(self.selected_service)
+            
+                    
+        tk.Button(add_mail_popup,text="add",command=add_entry).place(relx=0,rely=0.8,relheight=0.1)
+        
+        
+        add_mail_popup.transient(self.root)
+        add_mail_popup.grab_set
+
+    def scale_toplevel(self, window: tk.Toplevel, size: float):
         screen_width = window.winfo_screenwidth()
         screen_height = window.winfo_screenheight()
         width = int(screen_width * size)
         height = int(screen_height * size)
-        x = int((screen_width - width) *size)
-        y = int((screen_height - height) *size)
+        x = int((screen_width - width) * size)
+        y = int((screen_height - height) * size)
         window.geometry(f"{width}x{height}+{x}+{y}")
-    def add_mail(self):
-        pass
-            
-            
+
 
 if __name__ == "__main__":
     App()
